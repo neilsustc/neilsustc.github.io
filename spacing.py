f49b3f06485e56f06914c0280e048a9af5d33f06
Yu <583181285@qq.com>
2017-01-02 09:57:15 +0800

    spacing

---
+++ spacing.py
@@ -0,0 +1,20 @@
+import pangu
+import sys
+
+
+def main(argv):
+    file = argv[0]
+    with open(file, encoding='utf8') as r:
+        content = r.read()
+    with open(file, 'w', encoding='utf8') as w:
+        content = pangu.spacing(content)
+        # For markdown
+        if file.endswith('.md'):
+            content = content.replace(' ** ', '**') \
+                             .replace('"/>', '" />') \
+                             .replace(' “', '“') \
+                             .replace('” ', '”')
+        w.write(content)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
